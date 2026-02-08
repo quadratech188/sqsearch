aae8086d2fba78068db8f4e965cf1eaf56a91bc2
Quadratech188 <quadratech188@gmail.com>
2026-02-09 01:14:04 +0900

    checkpoint

--- src/db.rs
+++ src/db.rs
@@ -1,17 +1,26 @@
 
+use std::{fmt::format, iter};
+
 use indoc::indoc;
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
     #[error("SQLite error: {0}")]
-    SQLite(#[from] rusqlite::Error)
+    SQLite(#[from] rusqlite::Error),
+    #[error("Bad query:")]
+    BadQuery(Vec<String>)
 }
 
 
 pub fn prepare_db(conn: &rusqlite::Connection) -> Result<(), Error> {
 
-    conn.pragma_update(None, "journal_mode", "WAL")?;
+    // conn.pragma_update(None, "journal_mode", "WAL")?;
     conn.pragma_update(None, "synchronous", "normal")?;
+
+    {
+        let _guard = unsafe {rusqlite::LoadExtensionGuard::new(conn)?};
+        unsafe {conn.load_extension("./better-trigram.so", None::<&str>)}?;
+    }
 
     conn.execute_batch(indoc! {"
         BEGIN;
@@ -29,10 +38,27 @@ pub fn prepare_db(conn: &rusqlite::Connection) -> Result<(), Error> {
             inode INTEGER,
             id INTEGER,
             suffix TEXT,
-            PRIMARY KEY (parent_inode, suffix, id)
+            PRIMARY KEY (inode, suffix, id)
         ) WITHOUT ROWID;
 
-        CREATE INDEX IF NOT EXISTS suffix_idx ON suffix_array (suffix);
+        CREATE INDEX IF NOT EXISTS forwards_idx ON suffix_array (parent_inode, suffix);
+
+        CREATE VIRTUAL TABLE IF NOT EXISTS fts USING fts5(
+            name, tokenize = 'better_trigram', id
+        );
+
+        CREATE TRIGGER IF NOT EXISTS files_ai AFTER INSERT ON files BEGIN
+            INSERT INTO fts(id, name) VALUES (new.id, new.name);
+        END;
+
+        CREATE TRIGGER IF NOT EXISTS files_ad AFTER DELETE ON files BEGIN
+            INSERT INTO fts(fts, id, name) VALUES('delete', old.id, old.name);
+        END;
+
+        CREATE TRIGGER IF NOT EXISTS files_au AFTER UPDATE ON files BEGIN
+            INSERT INTO fts(fts, id, name) VALUES('delete', old.id, old.name);
+            INSERT INTO fts(id, name) VALUES (new.id, new.name);
+        END;
 
         COMMIT;
     "})?;
@@ -40,7 +66,8 @@ pub fn prepare_db(conn: &rusqlite::Connection) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn insert(tx: &rusqlite::Transaction, parent_inode: u64, inode: u64, name: &str) -> Result<(), Error> {
+pub fn insert(tx: &rusqlite::Transaction, parent_inode: u64, inode: u64, name: &str)
+-> Result<(), Error> {
     let mut file_insert = tx.prepare_cached(indoc! {"
         INSERT INTO files(inode, parent_inode, name) VALUES(?1, ?2, ?3)
         ON CONFLICT DO NOTHING
@@ -56,33 +83,90 @@ pub fn insert(tx: &rusqlite::Transaction, parent_inode: u64, inode: u64, name: &
     let mut suffix_insert = tx.prepare_cached(indoc! {"
         INSERT into suffix_array VALUES(?1, ?2, ?3, ?4)
     "})?;
-    for (i, _) in name.char_indices() { suffix_insert.execute((parent_inode as i64, inode as i64, id, &name[i..]))?;
+    for (i, _) in name.char_indices() {
+        suffix_insert.execute((parent_inode as i64, inode as i64, id, &name[i..]))?;
     }
     Ok(())
 }
 
-pub fn prepare_query<'a>(tx: &'a rusqlite::Transaction<'a>, segments: &[&str]) -> Result<rusqlite::CachedStatement<'a>, Error> {
-    let length = segments.len();
-    let mut query = format!(
-        "SELECT s{}.id FROM suffix_array AS s0 ",
-        length - 1);
-    for i in 1..length {
-        query += &format!(
-            "JOIN suffix_array AS s{} ON s{}.parent_inode = s{}.inode ",
-            i, i, i - 1);
-    }
-    query += "WHERE s0.suffix >= ? AND s0.suffix < ? ";
-    for i in 1..length {
-        query += &format!(
-            "AND s{}.suffix >= ? AND s{}.suffix < ? ",
-            i, i)
-    }
-    Ok(tx.prepare_cached(&query)?)
+fn estimate_specifity(query: &str) -> i64 {
+    query.len() as i64
 }
 
-// Return the data, not the iterator
-pub fn prepare_params(segments: &[&str]) -> Vec<String> {
-    segments.iter()
-        .flat_map(|&x| vec![x.to_string(), format!("{}\u{10FFFF}", x)])
-        .collect()
+pub fn prepare_query<'a>(tx: &'a rusqlite::Transaction<'a>, segments: &[&str])
+-> Result<(rusqlite::CachedStatement<'a>, Vec<String>), Error> {
+
+    let score = |x| estimate_specifity(segments[x]);
+
+    let Some(best_segment) = (0..segments.len())
+        .max_by_key(|&x| score(x)) else {
+        return Err(Error::BadQuery(segments.iter().map(|&x| x.into()).collect()))
+    };
+
+    // TODO: Allow multiple best segments if they're specific enough
+    
+    let mut l: i64 = best_segment as i64 - 1;
+    let mut r: i64 = best_segment as i64 + 1;
+    let length = segments.len() as i64;
+
+    let mut join_order = vec![best_segment];
+
+    // l underflows to 2^64 - 1 > segments.len()
+    while 0 <= l || r < length {
+        if length <= r ||
+            (0 <= l && r < length && score(l as usize) > score(r as usize)) {
+            join_order.push(l as usize);
+            l -= 1;
+        }
+        else {
+            join_order.push(r as usize);
+            r += 1;
+        }
+    }
+
+    let mut params: Vec<String> = vec![];
+
+    let header = format!(
+        "SELECT s{}.id FROM fts JOIN files AS s{} ON s{}.id = fts.id",
+        segments.len() - 1, join_order[0], join_order[0]);
+
+    /*
+    let header = format!(
+        "SELECT s{}.id FROM suffix_array AS s{}",
+        segments.len() - 1, join_order[0]);
+    */
+
+    let joins = (1..segments.len())
+        .map(|x| format!("suffix_array AS s{}", join_order[x]));
+
+    let parent_conds = (0..segments.len() - 1)
+        .map(|x| format!("s{x}.inode = s{y}.parent_inode", y = x+1));
+
+    let str_conds = (0..segments.len())
+        .map(|x| {
+            if x == join_order[0] {
+                params.push(format!("%{}%", segments[x]));
+                return format!("fts.name LIKE ?")
+            }
+            params.push(segments[x].into());
+            params.push(format!("{}\u{10FFFF}", segments[x]));
+
+            format!("s{x}.suffix >= ? AND s{x}.suffix < ?")
+        });
+
+    let first = iter::chain([header], joins).collect::<Vec<_>>().join(" JOIN ");
+    let second = iter::chain(parent_conds, str_conds).collect::<Vec<_>>().join(" AND ");
+
+    let query = if second == "" {
+        first
+    }
+    else {
+        format!("{first} WHERE {second}")
+    };
+
+
+    dbg!(&query);
+    dbg!(&params);
+
+    Ok((tx.prepare_cached(&query)?, params))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,65 @@
-use std::{os::unix::fs::MetadataExt, path};
+use std::{env, os::unix::fs::MetadataExt, path};
 
 use walkdir::WalkDir;
 
 mod db;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path = path::Path::new("./db");
+    let path = path::Path::new("./db.sqlite3");
     let mut db = rusqlite::Connection::open(path)?;
 
     db::prepare_db(&db)?;
 
-    let tx = db.transaction()?;
+    let args: Vec<_> = env::args().collect();
+    if args.len() > 1 && args[1] == "--index" {
+        let tx = db.transaction()?;
 
-    for entry in WalkDir::new("/usr/include") {
-        let entry = match entry {
-            Ok(e) => e,
-            Err(e) => {
-                eprintln!("Error walking: {}", e);
-                continue;
+        let mut cnt = 0;
+        for entry in WalkDir::new("/mnt/data") {
+            let entry = match entry {
+                Ok(e) => e,
+                Err(e) => {
+                    eprintln!("Error walking: {}", e);
+                    continue;
+                }
+            };
+
+            let metadata = entry.metadata()?;
+            let inode = metadata.ino();
+            let parent_inode = entry
+                .path()
+                .parent()
+                .unwrap()
+                .metadata()?
+                .ino();
+
+            db::insert(&tx, parent_inode, inode, entry.file_name().try_into()?)?;
+            cnt += 1;
+            if cnt % 1000 == 0 {
+                println!("{} {}", cnt, entry.path().display());
             }
-        };
+        }
 
-        let metadata = entry.metadata()?;
-        let inode = metadata.ino();
-        let parent_inode = entry
-            .path()
-            .parent()
-            .unwrap()
-            .metadata()?
-            .ino();
-
-        db::insert(&tx, parent_inode, inode, entry.file_name().try_into()?)?;
+        tx.commit()?;
     }
 
-    tx.commit()?;
+    if args.len() > 2 && args[1] == "--query" {
+        let tx = db.transaction()?;
+        {
 
-    let tx = db.transaction()?;
-    {
+            let segments: Vec<_> = args[2].split("/").collect();
+            let (mut query, params) = db::prepare_query(&tx, &segments)?;
 
-        let segments: Vec<_> = "a/kvm".split("/").collect();
-        let params = db::prepare_params(&segments);
-        let mut query = db::prepare_query(&tx, &segments)?;
-
-        let _: Vec<_> = query.query_map(
-            rusqlite::params_from_iter(params),
-            |x| {
-                let _ = dbg!(x.get::<usize, i64>(0));
-                Ok(())
-            })?.collect();
+            query.query_map(
+                rusqlite::params_from_iter(params),
+                |x| {
+                    Ok(x.get::<usize, i64>(0)?)
+                })?.for_each(|x| {
+                    let _ = dbg!(x);
+                });
+        }
+        tx.commit()?;
     }
-    tx.commit()?;
-
 
     Ok(())
 }
