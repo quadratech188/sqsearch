7af00646e5117df69c1120798c1049e583f06bbe
Quadratech188 <quadratech188@gmail.com>
2026-05-03 18:29:37 +0900

    refactor: Rewrite main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::path;
+use std::{error, path};
 
 use clap::Parser;
 
@@ -9,59 +9,89 @@ mod watcher;
 mod queryer;
 
 #[derive(clap::Parser, Debug)]
-struct Args {
-    #[arg(long, value_name = "FILE", default_value="/var/lib/sqsearch/db.sqlite3")]
-    db: path::PathBuf,
-
+struct CLI {
+    #[command(flatten)]
+    globals: GlobalArgs,
     #[command(subcommand)]
     command: Commands
 }
 
-#[derive(clap::Subcommand, Debug)]
-enum Commands {
-    Watch {
-        filesystem: path::PathBuf
-    },
-    Index {
-        dir: path::PathBuf
-    },
-    Query
+#[derive(clap::Args, Debug)]
+struct GlobalArgs {
+    #[arg(long, value_name = "FILE", default_value="/var/lib/sqsearch/db.sqlite3")]
+    db: path::PathBuf,
 }
 
-fn main() -> Result<(), String> {
+#[derive(clap::Subcommand, Debug, Clone)]
+enum Commands {
+    Watch(WatchArgs),
+    Index(IndexArgs),
+    Query(QueryArgs)
+}
+
+#[derive(clap::Args, Debug, Clone)]
+struct WatchArgs {
+    path: path::PathBuf
+}
+
+fn watch(globals: GlobalArgs, args: WatchArgs) -> Result<(), Box<dyn error::Error>> {
+    let mut conn = rusqlite::Connection::open_with_flags(
+        globals.db,
+        rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE
+    )?;
+
+    db::prepare_db(&conn)?;
+    watcher::watch(&args.path, &mut conn)?;
+
+    Ok(())
+}
+
+#[derive(clap::Args, Debug, Clone)]
+struct IndexArgs {
+    path: path::PathBuf
+}
+
+fn index(globals: GlobalArgs, args: IndexArgs) -> Result<(), Box<dyn error::Error>> {
+    let mut conn = rusqlite::Connection::open_with_flags(
+        globals.db,
+        rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE
+        | rusqlite::OpenFlags::SQLITE_OPEN_CREATE
+    )?;
+
+    db::prepare_db(&conn)?;
+    indexer::index(&mut conn, &args.path)?;
+
+    Ok(())
+}
+
+#[derive(clap::Args, Debug, Clone)]
+struct QueryArgs {
+
+}
+
+fn query(globals: GlobalArgs, _: QueryArgs) -> Result<(), Box<dyn error::Error>> {
+    let conn = rusqlite::Connection::open_with_flags(
+        globals.db,
+        rusqlite::OpenFlags::SQLITE_OPEN_READ_ONLY
+    )?;
+
+    db::prepare_db(&conn)?;
+    queryer::query(conn)?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn error::Error>> {
     env_logger::Builder::from_env(
         env_logger::Env::default()
         .default_filter_or("info")
     ).init();
 
-    let args = Args::parse();
-
-    let flags = match args.command {
-        Commands::Watch { filesystem: _ }
-            => rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE,
-        Commands::Index { dir: _ }
-            => rusqlite::OpenFlags::SQLITE_OPEN_READ_WRITE
-            | rusqlite::OpenFlags::SQLITE_OPEN_CREATE,
-        Commands::Query
-            => rusqlite::OpenFlags::SQLITE_OPEN_READ_ONLY
-    };
-
-    let mut db = rusqlite::Connection::open_with_flags(&args.db, flags).map_err(|e|
-        e.to_string())?;
-
-    db::prepare_db(&db).map_err(|e| e.to_string())?;
+    let args = CLI::parse();
 
     match args.command {
-        Commands::Watch { filesystem } => {
-            watcher::watch(&filesystem, &mut db).map_err(|e| e.to_string())?;
-        }
-        Commands::Index { dir } => {
-            indexer::index(&mut db, &dir).map_err(|e| e.to_string())?;
-        }
-        Commands::Query => {
-            queryer::query(db).map_err(|e| e.to_string())?;
-        }
+        Commands::Watch(x) => watch(args.globals, x),
+        Commands::Index(x) => index(args.globals, x),
+        Commands::Query(x) => query(args.globals, x)
     }
-
-    Ok(())
 }
